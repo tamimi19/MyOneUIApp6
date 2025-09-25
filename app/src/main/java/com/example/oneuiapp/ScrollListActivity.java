8d6e52f7d83ee17aff7820efd454fe7f091688e5
tamimi19 <mohamad.tamimii.82@gmail.com>
2025-09-25 12:39:16 +0300

    Update ScrollListActivity.java

--- app/src/main/java/com/example/oneuiapp/ScrollListActivity.java
+++ app/src/main/java/com/example/oneuiapp/ScrollListActivity.java
@@ -46,8 +46,6 @@ public class ScrollListActivity extends AppCompatActivity {
     private void setupToolbar() {
         toolbarLayout.setTitle(getString(R.string.scroll_list_title));
         toolbarLayout.setSubtitle(getString(R.string.scroll_list_subtitle));
-        toolbarLayout.setNavigationIcon(getDrawable(R.drawable.ic_samsung_back));
-        
         // Set toolbar to collapsed state by default (Samsung OneUI behavior)
         toolbarLayout.setExpandable(true);
         toolbarLayout.setExpanded(false, false);
